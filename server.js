461f8c742ea1ebc1bb54427f7a97467835fcf52f
radhakulkarni24 <radha.kulkarni024@gmail.com>
2024-08-25 09:01:17 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,4 @@
+//lets create API with express JS
+// We have methods like - get, post, put,delete - crud operation 
+//we can able to create Server using node Js but xpress provides easy way to create it.
+
